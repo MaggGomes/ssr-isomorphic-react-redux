21eeef1564335a3202603769c0343b0dabfc8b52
unknown <magg.slb@gmail.com>
2019-12-07 03:25:13 +0000

    initial commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>Home New New New Page</div>
+            <button onClick={() => console.log("hi")}>Click me</button>
+        </div>
+    )
+};
+
+export default Home;
---
+++ webpack.server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpackNodeExternal = require('webpack-node-externals');
+
+module.exports = {
+    // Inform webpack that we are building a bundle for nodeJS and not for the browser
+    target: 'node',
+
+    // Tell webpack the root file of the application
+    entry: './src/index.js',
+
+    // Tell webpack where to put the output file thta is generated
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+
+    externals: [webpackNodeExternal()],
+
+    // Tell webpack to run babel on every file ir runs through
+    module: {
+        rules: [
+          {
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+              presets: [
+                'react',
+                'stage-0',
+                ['env', { targets: { browsers: ['last 2 versions'] } }]
+              ]
+            }
+          }
+        ]
+      },
+};
